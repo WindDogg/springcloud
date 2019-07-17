49464a9556c6ec67df9e9a833b7d440eb7c1ac17
WindDogg <1150547471@qq.com>
2019-07-18 00:20:04 +0800

    增加服务链路追踪

--- product-data-service/src/main/java/com/xue/App.java
+++ product-data-service/src/main/java/com/xue/App.java
@@ -1,5 +1,6 @@
 package com.xue;
 
+import brave.sampler.Sampler;
 import cn.hutool.core.convert.Convert;
 import cn.hutool.core.thread.ThreadUtil;
 import cn.hutool.core.util.NetUtil;
@@ -7,6 +8,7 @@ import cn.hutool.core.util.NumberUtil;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 import java.util.Scanner;
 import java.util.concurrent.ExecutionException;
@@ -51,8 +53,12 @@ public class App {
         }
 
         new SpringApplicationBuilder(App.class).properties("server.port=" + port).run(args);
-
-
+    }
+    //配置Sampler抽样策略
+    @Bean
+    public Sampler dafaultSampler(){
+        //持续抽样
+        return Sampler.ALWAYS_SAMPLE;
     }
 }
 
