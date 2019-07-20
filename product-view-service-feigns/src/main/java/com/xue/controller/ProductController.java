87c07e60eeb2d3c74d9d5477aee7ae30a31b39eb
WindDogg <1150547471@qq.com>
2019-07-20 09:11:05 +0800

    配置客户端获取配置信息

--- product-view-service-feigns/src/main/java/com/xue/controller/ProductController.java
+++ product-view-service-feigns/src/main/java/com/xue/controller/ProductController.java
@@ -3,6 +3,8 @@ package com.xue.controller;
 import com.xue.pojo.Product;
 import com.xue.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,14 +12,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.List;
 
 @Controller
+@RefreshScope
 public class ProductController {
 
     @Autowired
     ProductService productService;
+
+    @Value("${version}")
+    String version;
+
     @RequestMapping("/products")
     public Object products(Model model){
         List<Product> list = productService.listProducts();
         model.addAttribute("ps",list);
+        model.addAttribute("version",version);
         return "products";
     }
 }
