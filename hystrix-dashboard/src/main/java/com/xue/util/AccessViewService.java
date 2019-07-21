27aae10f9a1ddf27def6b1f6311593f26bca9eaa
WindDogg <1150547471@qq.com>
2019-07-22 00:03:56 +0800

    增加多个视图服务的访问

--- hystrix-dashboard/src/main/java/com/xue/util/AccessViewService.java
+++ hystrix-dashboard/src/main/java/com/xue/util/AccessViewService.java
@@ -16,4 +16,13 @@ public class AccessViewService {
 
         }
     }
+
+    public static void access(int port){
+        try {
+            String html = HttpUtil.get(String.format("http://127.0.0.1:%d/products",port));
+            System.out.printf("%d 地址的视图服务访问成功，返回大小是 %d%n",port,html.length());
+        }catch (Exception e){
+            System.err.printf("%d 地址的视图服务无法访问%n",port);
+        }
+    }
 }
